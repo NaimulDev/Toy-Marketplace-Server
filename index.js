53e9c4489ffb4b691950ecde36685099930571e7
Nimul99 <htnimul99@gmail.com>
2023-05-21 13:02:18 +0600

    create post method route

--- index.js
+++ index.js
@@ -1,119 +1,3 @@
-// const express = require("express");
-// const app = express();
-// require("dotenv").config();
-// var cors = require("cors");
-// const port = process.env.PORT || 5000;
-
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-// const uri = `mongodb+srv://${process.env.USER}:${process.env.SECRET_KEY}@cluster0.gflxnku.mongodb.net/?retryWrites=true&w=majority`;
-
-// app.use(cors());
-// app.use(express.json());
-
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-
-//     const collectionToys = client.db("ToyTown").collection("toys");
-//     const collectionBookings = client.db("ToyTown").collection("bookings");
-
-//     app.get("/toys", async (req, res) => {
-//       const cursor = collectionToys.find();
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-
-//     app.get("/toys/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const options = {
-//         // Include only the `title` and `imdb` fields in the returned document
-//         projection: { title: 1, price: 1, service_id: 1, img: 1 },
-//       };
-//       const result = await collectionToys.findOne(query, options);
-//       res.send(result);
-//     });
-
-//     app.get("/bookings", async (req, res) => {
-//       let query = {};
-//       if (req.query?.email) {
-//         query = { email: req.query.email };
-//       }
-//       const result = await collectionBookings.find().toArray();
-//       res.send(result);
-//     });
-
-//     app.get("/toys", async (req, res) => {
-//       console.log("serial number 77", req.query);
-//       const query = { category_name: "Car Robot" };
-//       const cursor = collectionToys.find(query);
-//       const result = await cursor.toArray();
-//       res.send(result);
-//     });
-
-//     app.post("/bookings", async (req, res) => {
-//       const booking = req.body;
-//       const result = await collectionBookings.insertOne(booking);
-//       res.send(result);
-//     });
-
-//     app.put("/toys/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const filter = { _id: new ObjectId(id) };
-//       const options = { upsert: true };
-//       const updateToys = req.body;
-//       const Toys = {
-//         $set: {
-//           price: updateToys.price,
-//           quantity: updateToys.quantity,
-//           details: updateToys.details,
-//         },
-//       };
-//       const result = await collectionToys.updateOne(filter, Toys, options);
-//       res.send(result);
-//     });
-
-//     app.delete("/toys/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const query = { _id: new ObjectId(id) };
-//       const result = await collectionToys.deleteOne(query);
-//       res.send(result);
-//     });
-//     // app.delete("/bookings/:id", async(req, res) => {
-
-//     // })
-
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// app.get("/", (req, res) => {
-//   res.send("Toy Marketplace is running!");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Toy Marketplace server is running on port ${port}`);
-// });
-
-// ===========================
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -163,9 +47,9 @@ async function run() {
 
     /* new item add rout  */
     app.post("/toyProducts", async (req, res) => {
-      const newRobot = req.body;
-      console.log("new robot router", newRobot);
-      const result = await toyCollection.insertOne(newRobot);
+      const newToy = req.body;
+      console.log("new toy router", newToy);
+      const result = await toyCollection.insertOne(newToy);
       res.send(result);
     });
 
@@ -189,21 +73,21 @@ async function run() {
     });
 
     //sub category rout read
-    app.get("/carRobot", async (req, res) => {
+    app.get("/softToys", async (req, res) => {
       console.log("serial number 77", req.query);
-      const query = { category_name: "Car Robot" };
+      const query = { category: "Soft Toys" };
       const cursor = toyCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get("/electricRobot", async (req, res) => {
-      const query = { category_name: "Electric Robot" };
+    app.get("/rubberwood", async (req, res) => {
+      const query = { category: "Rubberwood" };
       const cursor = toyCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get("/flyingRobot", async (req, res) => {
-      const query = { category_name: "Flying Robot" };
+    app.get("/girlsCloth", async (req, res) => {
+      const query = { category: "Girl's Cloth" };
       const cursor = toyCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
